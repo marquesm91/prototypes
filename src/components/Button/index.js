06c330a7ff4289a2a176214479e561f34c60fa88
Matheus Marques <marquesm91@gmail.com>
2018-05-01 17:41:32 -0300

    feat: Add eslint, prettier, styled comps and first dumb component

---
+++ src/components/Button/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Button as B } from './styles';
+
+const Button = () => <B>Click me</B>;
+
+export { Button };
---
+++ src/components/Button/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  border-radius: 3px;
+  padding: 10px;
+  box-sizing: content-box;
+`;
